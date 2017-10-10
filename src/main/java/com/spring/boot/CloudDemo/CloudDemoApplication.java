c098be3212a37fee81ccf735b9e4bb48902a24a7
Subramaniam, Nirmalkumar <nirmalkumar.subramaniam@thermofisher.com>
2017-10-10 11:19:34 -0400

    final commit

---
+++ src/main/java/com/spring/boot/CloudDemo/CloudDemoApplication.java
@@ -0,0 +1,14 @@
+package com.spring.boot.CloudDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CloudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudDemoApplication.class, args);
+	}
+}
